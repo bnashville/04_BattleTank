1503625f9c7aeda786f99eb66f88b780d192fb75
DESKTOP-GTB8VQ5\Brian <bcofer@gmail.com>
2018-07-25 15:18:27 -0500

    130

--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -1,23 +1,23 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
-#include "CoreMinimal.h"
-#include "GameFramework/PlayerController.h"
 #include "Tank.h"
-#include "TankPlayerController.generated.h"
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "TankAIController.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class BATTLETANK_API ATankPlayerController : public APlayerController
+class BATTLETANK_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
-	
-public:
+
 	ATank* GetControlledTank() const;
-	
+
 	void BeginPlay() override;
+	
+	
 	
 };
